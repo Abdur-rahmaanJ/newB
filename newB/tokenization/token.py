ca66e487e3fe2c137662387a12159820fc6be809
Michael Ermishin <ermichin1999@gmail.com>
2017-02-28 15:57:52 +0300

    Update token.py
    Added sub_group for token

--- newB/tokenization/token.py
+++ newB/tokenization/token.py
@@ -5,12 +5,14 @@ class Token:
     def __init__(self, token_type, value):
         self.value = value
 
-        self.type = token_type
+        self.group = token_type[0]
+        self.sub_group = self.group + '-' + token_type[1]
         pass
 
     def __str__(self):
-        return '{type} {quotes}{value}{quotes}'.format(
-            type=self.type,
+        return '{sub_type} {quotes}{value}{quotes}'.format(
+            type=self.group,
+            sub_type=self.sub_group,
             value=string_to_printable(self.value),
-            quotes='`' if self.type == 'string' else ''
+            quotes='`' if self.group == 'string' else ''
         )
